a459a48b9ebec1f9603816ac6a327a32b5c67a0c
Pramila Tawari <pramila.tawari2607@gmail.com>
2020-03-03 15:26:43 +0530

    Data Class Example DOne

--- src/com/bridgelabz/oop/DataClass.kt
+++ src/com/bridgelabz/oop/DataClass.kt
@@ -5,12 +5,23 @@ fun main(args: Array<String>) {
     var student1 = Student("Pramila",5)
     var student2 = Student("Pramila",5)
 
-    if(student1 == student2){
+    println(student1)  // By default it is toString()
+                     // Output without data keyword --com.bridgelabz.oop.Student@816f27d
+                  // Output with data keyword -- Student(name=Pramila, id=5)
+    if(student1 == student2) // Here we are actually comparing the object reference
+        // both object are in different heap memory
+    {
         println("Equal")
     }else{
         println("Not Equal")
     }
+
+    var newUser = student1.copy(name="Sagar")  // Copying the Object and we can also change the values
+    println(newUser)
 }
-class Student(name : String , id : Int){
+
+// The purpose of data class is to deal with data and not object
+// so our output will me Equal and without data output will be Not Equal
+data class Student(var name : String , var id : Int){
 
 }
